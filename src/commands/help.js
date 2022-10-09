777b8494d4eb7c2320ea0ded9016d9be316ae443
C-D-Lewis <Bonsitm@gmail.com>
2022-10-09 16:29:46 +0100

    Update

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,13 +6,13 @@
  */
 module.exports = (interaction) => interaction.reply(`
 🎲 \`/roll <d>\` - Roll a <d> die (using CRNG)
+📖 \`/search <query>\` - Search Roll20.net compendium
 🔊 \`/sound <name>/list/stop\` - Play/stop/list sound files
 🎶 \`/music <name>/list/stop\` - Play/stop/list music files
-📖 \`/search <query>\` - Search Roll20.net compendium
 🎲 \`/rolltohit <result>\` - See if your hit will land...
 ❔ \`/ask <question>\` - Ask me a yes/no question
 ❓ \`/help\` - Show this information
 🤖 \`/ping\` - Ping the bot server
 
-⚙️ \`https://github.com/C-D-Lewis/discord-bot-hom\`
+📜 <https://github.com/C-D-Lewis/discord-bot-hom>
 `);
