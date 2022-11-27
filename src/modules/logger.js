c5f40f4d06c937182cf7561913fc8422f778f10d
Chris Lewis <Bonsitm@gmail.com>
2022-11-27 21:50:09 +0000

    log to file

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,3 +1,24 @@
+const fs = require('fs');
+
+/** Log file name */
+const FILE_PATH = 'discord-bot-norren.txt';
+
+/**
+ * Write a log line to file.
+ *
+ * @param {string} msg - Message to write.
+ */
+ const writeToFile = (msg) => {
+  let stream;
+  if (!fs.existsSync(FILE_PATH)) {
+    stream = fs.createWriteStream(FILE_PATH, { flags: 'w' });
+    stream.end(`New log file!\n`);
+  }
+
+  stream = fs.createWriteStream(FILE_PATH, { flags: 'a' });
+  stream.end(`${msg}\n`);
+};
+
 /**
  * Log a structured message.
  *
@@ -7,7 +28,11 @@ const log = (msg) => {
   const [date, timeStr] = new Date().toISOString().split('T');
   const [time] = timeStr.split('.');
   const isObject = typeof msg === 'object';
-  console.log(`[${date} ${time}] ${isObject ? '' : msg}`);
+  const logMsg = `[${date} ${time}] ${isObject ? '' : msg}`;
+  console.log(logMsg);
+
+  // To file
+  writeToFile(logMsg);
 
   // Could be complex object
   if (isObject) console.log(msg);
