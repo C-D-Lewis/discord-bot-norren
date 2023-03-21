257459faa3ecc03d0b0869106c3529844367ab2e
C-D-Lewis <Bonsitm@gmail.com>
2023-03-21 21:43:35 +0000

    Refactor tts service

--- src/commands/say.js
+++ src/commands/say.js
@@ -1,10 +1,7 @@
-const fetch = require('node-fetch');
-const { writeFileSync } = require('fs');
 const { execSync } = require('child_process');
 const { replyHidden } = require('../modules/discord');
 const { getVoiceAgent } = require('../modules/voice');
-const { elevenlabsApiKey, elevenlabsVoiceId } = require('../../config.json');
-const { log } = require('../modules/logger');
+const { generateSpeech, convertSpeech } = require('../modules/tts');
 
 /** Path to speech file without extension */
 const FILE_NO_EXT = `${__dirname}/../../sounds/speech`;
@@ -36,37 +33,14 @@ module.exports = async (interaction) => {
   await replyHidden(interaction, { content: `Say: _${message}_\n\nGenerating...` });
   inProgress = true;
 
-  execSync(`rm -f ${FILE_NO_EXT}.*`);
-
   try {
     // Request sound file
-    log(`Requesting speech: ${message}`);
-    const url = `https://api.elevenlabs.io/v1/text-to-speech/${elevenlabsVoiceId}`;
-    const res = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'xi-api-key': elevenlabsApiKey,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        text: message,
-        voice_settings: {
-          stability: 1.0,
-          similarity_boost: 1.0,
-        },
-      }),
-    });
-    log(`Response: ${res.status}`);
-    if (res.status !== 200) throw new Error(res.text());
-
-    const buffer = await res.buffer();
-    writeFileSync(`${FILE_NO_EXT}.mpg`, buffer);
-    log('Wrote speech file');
+    execSync(`rm -f ${FILE_NO_EXT}.*`);
+    await generateSpeech(message);
 
     // Convert to opus
     await interaction.editReply(`Say: _${message}_\n\nConverting...`);
-    execSync(`ffmpeg -i ${FILE_NO_EXT}.mpg ${FILE_NO_EXT}.opus`);
-    log('Converted speech file to opus');
+    await convertSpeech();
 
     // Play it!
     const voiceAgent = getVoiceAgent(voice);
@@ -80,7 +54,6 @@ module.exports = async (interaction) => {
   }
 
   execSync(`rm -f ${FILE_NO_EXT}.*`);
-
   inProgress = false;
   return undefined;
 };
---
+++ src/modules/tts.js
@@ -0,0 +1,52 @@
+const fetch = require('node-fetch');
+const { writeFileSync } = require('fs');
+const { execSync } = require('child_process');
+const { elevenlabsApiKey, elevenlabsVoiceId } = require('../../config.json');
+const { log } = require('./logger');
+
+/** Path to speech file without extension */
+const FILE_NO_EXT = `${__dirname}/../../sounds/speech`;
+
+/**
+ * Generate and store speech as a file.
+ *
+ * @param {string} message - Message to generate as speech.
+ * @returns {Promise<void>}
+ */
+const generateSpeech = async (message) => {
+  log(`Requesting speech: ${message}`);
+  const url = `https://api.elevenlabs.io/v1/text-to-speech/${elevenlabsVoiceId}`;
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'xi-api-key': elevenlabsApiKey,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      text: message,
+      voice_settings: {
+        stability: 1.0,
+        similarity_boost: 1.0,
+      },
+    }),
+  });
+  log(`Response: ${res.status}`);
+  if (res.status !== 200) throw new Error(res.text());
+
+  const buffer = await res.buffer();
+  writeFileSync(`${FILE_NO_EXT}.mpg`, buffer);
+  log('Wrote speech file');
+};
+
+/**
+ * Convert local speech file.
+ */
+const convertSpeech = () => {
+  execSync(`ffmpeg -i ${FILE_NO_EXT}.mpg ${FILE_NO_EXT}.opus`);
+  log('Converted speech file to opus');
+};
+
+module.exports = {
+  generateSpeech,
+  convertSpeech,
+};
